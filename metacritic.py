ba1b0636cacd0e3c85fc6f2e38634b8b8226a016
Hrishabh Joshi <54844760+joshi008@users.noreply.github.com>
2020-07-07 19:17:05 +0530

    Add files via upload

--- metacritic.py
+++ metacritic.py
@@ -13,7 +13,7 @@ search_page = BeautifulSoup(requests.get(url, headers=headers).text, 'lxml')
 first_result =  search_page.find('li', {"class":"result first_result"})
 
 link = first_result.find('a').get('href')
-link = "https://www.metacritic.com/" + link
+link = "https://www.metacritic.com/" + link + "/user-reviews"
 
 print(link)
 # print(first_result)
@@ -23,3 +23,17 @@ movie_name = movie_name.rstrip()
 movie_name = movie_name.lstrip()
 
 print(movie_name)
+
+
+review = BeautifulSoup(requests.get(link, headers=headers).text, 'lxml')
+
+with open("metacritic_reviews.txt", 'w') as review_file:
+    num_review = 1
+    print(review)
+    for reviews in review.find_all("div", {"class" : "review_body"}):
+        r = ''.join(reviews.findAll(text=True))
+        print(r)
+        review_file.write(r)
+        review_file.write("\n")
+        review_file.write(str(num_review))
+        num_review += 1
